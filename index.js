250a943cd594fe95cd54674dcd8c7c9d71bb9fed
Sylva <sylvaintagnabou@gmail.com>
2022-08-29 00:31:13 +0000

    dont work

--- index.js
+++ index.js
@@ -3,34 +3,45 @@ const ctx = canvas.getContext('2d');
 
 canvas.width =500;
 canvas.height = 700;
-const explosion = [];
+const explosions = [];
 let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x,y){
-        this.x = x; this.y = y;
-        this.sprideWidth = 200;
-        this.sprideHeight =179;
-        this.width = this.sprideWidth / 2;
-        this.height = this.sprideHeight / 2;
-        this.image = new Image;
+        this.x = x; 
+        this.y = y;
+        this.spriteWidth = 200;
+        this.spriteHeight =179;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.image = new Image();
         this.image.src = 'boom.png';
-        this.frame = 0
+        this.frame = 0;
     }
     update() {
         this.frame++;
     }
     draw(){
-        ctx.drawImage(this.image,this.sprideWidth * this.frame, 0 , this.sprideWidth , spriteHeight , 
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0 , this.spriteWidth , spriteHeight , 
             this.x , this.y , this.width , this.height);
     }
 }
 
-ctx.fillStyle = 'white';
 
 window.addEventListener('click', function(e){
-    console.log(e);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(e.x, e.y, 50, 50);
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+    console.log(explosions)
 });
 
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(let i = 0; i < explosions.length; i++){
+        explosions[i].update();
+        explosions[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
